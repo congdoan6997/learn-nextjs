74d1117df1b401c8e5e133767357a1d48e6e5780
PhDBui <congdoan6997@gmail.com>
2023-08-18 20:21:28 +0700

    nextjs layouts and components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>&#169; 2023 PhdBui. All rights reserved.</div>
+    </div>
+  );
+};
+
+export default Footer;
