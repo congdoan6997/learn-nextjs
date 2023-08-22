7bfdff20e6330ab1e09cc97e252e133e77fca3f1
PhDBui <congdoan6997@gmail.com>
2023-08-22 19:55:36 +0700

    fetch data in client and use swr library

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import styles from "./page.module.css";
+import useSWR from "swr";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  // const [data, setData] = useState([]);
+  // const [err, setErr] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+  //       cache: "no-cache",
+  //     });
+  //     if (!res.ok) {
+  //       setErr(true);
+  //     }
+  //     const data = await res.json();
+  //     setData(data);
+  //     setIsLoading(false);
+  //   };
+  //   getData();
+  // }, []);
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+  console.log(data);
+  return <div>Test</div>;
 };
 
 export default Dashboard;
