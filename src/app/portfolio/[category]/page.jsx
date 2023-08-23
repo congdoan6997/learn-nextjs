d4bbd7e1ab45aa10468adc778357e0b7cb0cf8ca
PhDBui <congdoan6997@gmail.com>
2023-08-23 09:14:29 +0700

    get data from file json

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,52 +2,41 @@ import React from "react";
 import styles from "./page.module.css";
 import Button from "@/components/botton/Button";
 import Image from "next/image";
+import { items } from "./data.js";
+import { notFound } from "next/navigation";
+
+const getData = (cat) => {
+  const data = items[cat];
+  if (data) {
+    return data;
+  }
+  return notFound();
+};
 
 const Category = ({ params }) => {
   // console.log(params);
+  const data = getData(params.category);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit
-            iusto magni dolorem similique nostrum maiores minima, aliquid sunt
-            perspiciatis aliquam a mollitia deleniti reiciendis eum eligendi
-            non! Architecto, necessitatibus voluptatum.
-          </p>
-          <Button className={styles.btn} url="#" text="See more"></Button>
+      {data.map((item) => (
+        <div className={styles.item}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button className={styles.btn} url="#" text="See more"></Button>
+          </div>
+          <div className={styles.imgContent}>
+            <Image
+              alt="image catefory portfolio"
+              className={styles.img}
+              src={item.image}
+              fill={true}
+            ></Image>
+          </div>
         </div>
-        <div className={styles.imgContent}>
-          <Image
-            alt="image catefory portfolio"
-            className={styles.img}
-            src="/websites.jpg"
-            fill={true}
-          ></Image>
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit
-            iusto magni dolorem similique nostrum maiores minima, aliquid sunt
-            perspiciatis aliquam a mollitia deleniti reiciendis eum eligendi
-            non! Architecto, necessitatibus voluptatum.
-          </p>
-          <Button className={styles.btn} url="#" text="See more"></Button>
-        </div>
-        <div className={styles.imgContent}>
-          <Image
-            alt="image catefory portfolio"
-            className={styles.img}
-            src="/websites.jpg"
-            fill={true}
-          ></Image>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
